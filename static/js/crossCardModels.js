6a16c2b6c1698e48d1be5e657b03894bfee05da0
Sawyer <sawyerjhood@gmail.com>
2014-08-06 12:25:50 -0400

    Refractored code to be organized. Ready to work on server.

---
+++ static/js/crossCardModels.js
@@ -0,0 +1,161 @@
+(function(exports){
+  exports.Card = function Card(value, type) {
+      this.value = value;
+      this.type = type;
+    }
+
+    exports.Player = function Player(type){
+      this.type = type;
+      this.currentCard = null;
+      this.reserveCard = null;
+    }
+
+    exports.Board = function Board(deck, players, playerIndex, board) {
+      
+
+      this.clearBoard = function() {
+        newBoard = [];
+        for(var i = 0; i < this.size; i++) {
+          newBoard.push([]);
+          for(var j = 0; j < this.size; j++) {
+            newBoard[i].push(null);
+          }
+          
+        }
+        return newBoard;
+      }
+
+      this.size = board ? board.length : 3;
+      this.board = board || this.clearBoard();
+      this.deck = deck;
+      this.players = players || [new Player('-'), new Player('|')];
+      this.playerIndex = playerIndex || 0;
+
+      this.isOccupied = function(row, col) {
+        if(this.board.length <= row || this.board[row].length <= col)
+          return true;
+        return this.board[row][col] != null;
+      }
+
+      this.isBoardFull = function() {
+        for(var i = 0; i < this.size; i++) {
+          for(var j = 0; j < this.size; j++) {
+            if(!this.isOccupied(i,j))
+              return false;
+          }
+        }
+        return true;
+      }
+
+      this.getRowValue = function(row) {
+        var total = 0;
+        for(var i = 0; i < this.board[row].length; i++) {
+          if(!this.isOccupied(row, i)) {
+            continue;
+          } else if (this.board[row][i].type == "+" || this.board[row][i].type == "-") {
+            total += this.board[row][i].value;
+          } else if (this.board[row][i].type == "*"){
+            return 0;
+          }
+        }
+        return total;
+      }
+
+      this.getColValue = function(col) {
+        var total = 0;
+        for(var i = 0; i < this.board.length; i++) {
+          if(!this.isOccupied(i, col)) {
+            continue;
+          } else if (this.board[i][col].type == "+" || this.board[i][col].type == "|") {
+            total += this.board[i][col].value;
+          } else if (this.board[i][col].type == "*"){
+            return 0;
+          }
+        }
+        return total;
+      }
+
+      this.placeCard = function(card, row, col) {
+        if (this.isOccupied(row, col))
+          return false;
+        this.board[row][col] = card;
+        return true;
+      }
+
+      this.getCard = function(row, col) {
+        return this.board[row][col];
+      }
+
+      this.getSortedColVaules = function() {
+        var scores = [];
+        for(var i = 0; i < this.size; i++) {
+          scores.push(this.getColValue(i));
+        }
+        scores.sort(function(a,b){return b-a});
+        return scores;
+      }
+
+      this.getSortedRowVaules = function() {
+        var scores = [];
+        for(var i = 0; i < this.size; i++) {
+          scores.push(this.getRowValue(i));
+        }
+        scores.sort(function(a,b){return b-a});
+        return scores;
+      }
+
+      this.getWinner = function() {
+        if (this.isBoardFull()) { 
+          var colVals = this.getSortedColVaules();
+          console.log(colVals);
+          var rowVals = this.getSortedRowVaules();
+          for(var i = 0; i < this.size; i++){
+            if(colVals[i] > rowVals[i])
+              return "|";
+            if(rowVals[i] > colVals[i])
+              return "-";
+          }
+        }
+        return null;
+      }
+
+      this.nextTurn = function() {
+      this.playerIndex++;
+      if (this.playerIndex >= this.players.length)
+        this.playerIndex = 0;
+      if (this.getCurrentPlayer().reserveCard == null) {
+        this.getCurrentPlayer().currentCard = this.deck.pop();
+      } else {
+        this.getCurrentPlayer().currentCard = this.getCurrentPlayer().reserveCard;
+        this.getCurrentPlayer().reserveCard = null;
+      }
+    }
+
+    this.getCurrentPlayer = function () {
+      return this.players[this.playerIndex]; 
+    };
+
+    this.playCard = function (row, col) {
+        var didPlace = this.placeCard(this.getCurrentPlayer().currentCard, row, col);
+        if(didPlace)
+          this.getCurrentPlayer().currentCard = null;
+        return didPlace;
+    }
+
+    this.reserve = function() {
+      var myPlayer = this.getCurrentPlayer();
+      if(myPlayer.reserveCard == null) {
+        myPlayer.reserveCard = myPlayer.currentCard;
+        myPlayer.currentCard = this.deck.pop();
+        return true;
+      } 
+      return false;
+
+    }
+  }
+
+  exports.shuffle = function shuffle(o){ 
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
+})(typeof exports === 'undefined'? this['crossCardModels']={}: exports); 
---
+++ static/js/crosscard.js
@@ -0,0 +1,88 @@
+angular.module('crossCardApp', ['ngRoute'])
+.controller('BoardController', ['$scope', '$http', 'baseDeck', function($scope, $http, baseDeck){
+
+
+  baseDeck = baseDeck.data;
+  var Player = crossCardModels.Player;
+  var Board = crossCardModels.Board;
+  var Card = crossCardModels.Card;
+  var shuffle = crossCardModels.shuffle;
+  
+
+  function playRandomCards (){
+    var types = ["+","-","|",]
+    for(var i = 0; i < $scope.gameBoard.size; i++) {
+      for(var j = 0; j < $scope.gameBoard.size; j++) {
+        $scope.gameBoard.placeCard($scope.deck.pop(), i, j);
+      }
+    }
+  }
+
+  $scope.getCurrentPlayer = function () {
+    return $scope.gameBoard.getCurrentPlayer(); 
+  };
+
+  $scope.playCard = function (row, col) {
+    if($scope.gameBoard.playCard(row, col)) {
+      if($scope.gameBoard.isBoardFull()) {
+        $scope.gameOver = true;
+        $scope.winner = $scope.gameBoard.getWinner();
+      } else {
+      $scope.switchTurn = true;
+      $scope.gameBoard.nextTurn();
+      }
+    } else {
+      alert("Choose another location.");
+    }
+  }
+
+  $scope.newGame = function() {
+    currentPlayerIndex = 0;
+    $scope.players = [new Player('-'), new Player('|')];
+    $scope.deck = shuffle(baseDeck.slice(0));
+    $scope.gameBoard = new Board(deck, players);
+    $scope.gameOver = false;
+  }
+
+  $scope.reserve = function() {
+    
+    if(!$scope.gameBoard.reserve()) {
+      alert("Can't reserve");
+    } 
+  }
+
+  $scope.switchTurnOff = function() {
+    $scope.switchTurn = false;
+  }
+
+  $scope.gameOver = false;
+  $scope.switchTurn = false;
+  var deck = shuffle(baseDeck.slice(0));
+  var players = [new Player('-'), new Player('|')];
+  $scope.gameBoard = new Board(deck, players);
+  $scope.gameBoard.nextTurn();
+  
+
+  
+
+}]).
+config(function($routeProvider){
+  $routeProvider.when('/', {
+    templateUrl: 'static/partials/localMultiplayer.html',
+    controller: 'BoardController',
+    resolve: {
+      baseDeck : function ($http) {
+          return $http ({method: 'GET', url: 'deck.json'}).
+          success(function(data, status, headers, config) {
+            console.log(data);
+            return data;
+          }).
+          error(function(data, status, headers, config) {
+              console.log(status);
+          })
+        }
+    }
+
+  });
+});
+
